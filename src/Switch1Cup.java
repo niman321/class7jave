8c7dc056f57608a2ecf8d45901826e56e0fe49c8
nima321 <niman8900@gmail.com>
2023-01-13 01:30:27 +0000

    version1

---
+++ src/Switch1Cup.java
@@ -0,0 +1,18 @@
+public class Switch1Cup {
+    public static void main(String[] args) {
+        int worldCub=2022;
+        switch (worldCub){
+            case 2022:
+                System.out.println("argintina");
+                break;
+            case 2018:
+                System.out.println("france");
+                break;
+            case 2014:
+                System.out.println("germany");
+                break;
+            default:
+                System.out.println("persia");
+        }
+    }
+}
